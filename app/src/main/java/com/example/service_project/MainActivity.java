482ef09ef3e3a63440bbb852575bde3cf497fdd9
pers <lsj622@naver.com>
2021-01-21 02:47:36 +0900

    update : matching user_id and store device_id

--- app/src/main/java/com/example/service_project/MainActivity.java
+++ app/src/main/java/com/example/service_project/MainActivity.java
@@ -11,8 +11,10 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,13 +23,21 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -35,6 +45,8 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button ok;
+    EditText user_id;
+    String deviceId;
     private final int PERMISSION_REQUEST_CODE = 200;
     final FirebaseFirestore db =FirebaseFirestore.getInstance();
 
@@ -48,36 +60,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if ( ContextCompat.checkSelfPermission( this, Manifest.permission.READ_PHONE_STATE )
                 != PackageManager.PERMISSION_GRANTED ) {
-            String deviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-            Map<String, Object> ID = new HashMap<>();
-            ID.put("device_id", deviceId);
-
-            db.collection("USERS").document("test1")
-                    .set(ID, SetOptions.merge())
-                    .addOnSuccessListener(new OnSuccessListener<Void>(){
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            //Log.d("debug", "latitude : " + String.valueOf(latitude));
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-
-                        }
-                    });
+            deviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
         }
 
         final Intent intent = new Intent(this.getApplication(), com.example.service_project.BackgroundLocationService.class);
         this.getApplication().startService(intent);
         this.getApplication().bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
 
+        user_id = (EditText) findViewById(R.id.editText);
         ok = (Button) findViewById(R.id.ok);
         ok.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Log.d("debug", "user_id : "+ user_id.getText().toString());
+        db.collection("USERS")
+                .whereEqualTo("user_id" ,  user_id.getText().toString() )
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        //Log.d("debug",   " db에서 얻어온 데이터 => " );
+                        String path = null;
+                        if(task.isSuccessful()){
+                            QuerySnapshot document = task.getResult();
+                            for (QueryDocumentSnapshot i : task.getResult()) {
+                                path = i.getId();
+                            }
+                                Map<String, Object> ID = new HashMap<>();
+                                ID.put("device_id", deviceId);
+                                db.collection("USERS").document(path)
+                                        .set(ID, SetOptions.merge())
+                                        .addOnSuccessListener(new OnSuccessListener<Void>(){
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                //Log.d("debug", "latitude : " + String.valueOf(latitude));
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+
+                                            }
+                                        });
+
+                        }else {
+                            Log.d("debug", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
         Intent intent = new Intent(getApplicationContext(), UsingActivity.class);
         startActivity(intent);
         startTracking();
